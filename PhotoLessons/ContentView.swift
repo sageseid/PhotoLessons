fbdf65dd5f2cb368443d1ecb7816671efc070b25
Noel-96 <noelorion69@gmail.com>
2023-02-11 21:50:43 +0100

    Initial Commit

---
+++ PhotoLessons/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PhotoLessons
+//
+//  Created by Noel Obaseki on 11/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoLessons/PhotoLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLessonsApp.swift
+//  PhotoLessons
+//
+//  Created by Noel Obaseki on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
